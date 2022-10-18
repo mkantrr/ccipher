e3f5b60c1b59ad7062d0558342754f80c71e2549
jaywynes <jayden.wynes@gmail.com>
2022-10-18 16:59:03 -0400

    Updated augustus for similarity

--- augustus.c
+++ augustus.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "caesar.h"
 
-char *augustus_encrypt(char *message, char *key) {
+char *augustus_encrypt(char *plain, char *key) {
 
 }
 
-char *augustus_decrypt(char *message, char *key) {
-    
+char *augustus_decrypt(char *cipher, char *key) {
+
 }
--- augustus.h
+++ augustus.h
@@ -1,6 +1,6 @@
 #ifndef _AUGUSTUS_H
 #define _AUGUSTUS_H
-char *augustus_encrypt(char *message, char *key);
-char *augustus_decrypt(char *message, char *key);
+char *augustus_encrypt(char *plain, char *key);
+char *augustus_decrypt(char *cipher, char *key);
 
-#endif
+#endif // _AUGUSTUS_H
