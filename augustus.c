30fc299d581c415047ef4463d38afa61a5e81042
jaywynes <jayden.wynes@gmail.com>
2022-10-18 16:52:42 -0400

    Initialized Augustus files, creatd .h for matt

--- augustus.c
+++ augustus.c
@@ -1,1 +1,11 @@
-asdklf
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *augustus_encrypt(char *message, char *key) {
+
+}
+
+char *augustus_decrypt(char *message, char *key) {
+    
+}
--- augustus.h
+++ augustus.h
@@ -1,1 +1,6 @@
-sad;fklasjd
+#ifndef _AUGUSTUS_H
+#define _AUGUSTUS_H
+char *augustus_encrypt(char *message, char *key);
+char *augustus_decrypt(char *message, char *key);
+
+#endif
