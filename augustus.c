51bf627cc58468b38934b8d48fbb4da92819e8f4
jaywynes <jayden.wynes@gmail.com>
2022-10-22 23:02:23 -0400

    Augustus full function, cleaned of tests

--- augustus.c
+++ augustus.c
@@ -2,24 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+int wrapAround(int start, int key) {
+    int wrap;
+    if ((start-key) < 0) {
+        wrap = 26 - abs(start-key);
+    } else {
+        wrap = start-key;
+    }
+    return wrap;
+}
+
 char caesarConvert(char input, int key) {
     int newPos;
     char currentChar = input;
     if (currentChar > 64 && currentChar < 91) {
-        newPos = currentChar-64;
-        newPos = ((newPos+key)%26)+64;
+        newPos = currentChar-65;
+        newPos = ((newPos+key)%26)+65;
 
     } else if (currentChar > 96 && currentChar < 123) {
-        newPos = currentChar - 96;
-        newPos = ((newPos+key)%26)+96;
+        newPos = currentChar - 97;
+        newPos = ((newPos+key)%26)+97;
 
     } else if (currentChar > 32 && currentChar < 59){
-        newPos = currentChar - 32;
-        newPos = ((newPos+key)%26)+32;
+        newPos = currentChar - 33;
+        newPos = ((newPos+key)%26)+33;
 
     } else if (currentChar == 32) {
         newPos = 32;
     }
+
 
     return newPos;
     
@@ -29,16 +40,19 @@ char caesarRevert(char input, int key) {
     int newPos;
     char currentChar = input;
     if (currentChar > 64 && currentChar < 91) {
-        newPos = currentChar-64;
-        newPos = ((newPos-key)%26)+64;
+        newPos = currentChar-65;
+        newPos = wrapAround(newPos, key);
+        newPos = ((newPos)%26)+65;
 
     } else if (currentChar > 96 && currentChar < 123) {
-        newPos = currentChar - 96;
-        newPos = ((newPos-key)%26)+96;
+        newPos = currentChar - 97;
+        newPos = wrapAround(newPos, key);
+        newPos = ((newPos)%26)+97;
 
     } else if (currentChar > 32 && currentChar < 59){
-        newPos = currentChar - 32;
-        newPos = ((newPos-key)%26)+32;
+        newPos = currentChar - 33;
+        newPos = wrapAround(newPos, key); 
+        newPos = ((newPos)%26)+33;
 
     } else if (currentChar == 32) {
         newPos = 32;
@@ -48,33 +62,13 @@ char caesarRevert(char input, int key) {
 }
 
 
-char caesarRevert(char input, int key) {
-    int newPos;
-    char currentChar = input;
-    if (currentChar > 64 && currentChar < 91) {
-        newPos = currentChar-64;
-        newPos = ((newPos+key)%26)+64;
-
-    } else if (currentChar > 96 && currentChar < 123) {
-        newPos = currentChar - 96;
-        newPos = ((newPos+key)%26)+96;
-
-    } else if (currentChar > 32 && currentChar < 59){
-        newPos = currentChar - 32;
-        newPos = ((newPos-key)%26)+32;
-
-    } else if (currentChar == 32) {
-        newPos = 32;
-    }
-
-    return newPos;
-}
 
 char *augustus_encrypt(char *plain, char *key) {
     int plainLen = strlen(plain);
     int keyLen = strlen(key);
     int intKey = atoi(key);
     char *encrypted= malloc(sizeof(plain));
+
     
     /*
     Step one, Caesar Conversion
@@ -94,11 +88,14 @@ char *augustus_encrypt(char *plain, char *key) {
     for (int i = 0; i<plainLen; i++) {
         char currentChar = encrypted[i];
         char newChar;
-        int curKey = atoi(key[i%keyLen]);
+        char temp;
+        temp = key[i%keyLen];
+        int val = temp - 48;
 
-        newChar = caesarConvert(currentChar, curKey);
+        newChar = caesarConvert(currentChar, val);
         encrypted[i] = newChar;        
     }
+    return encrypted;
 
     
 }
@@ -110,19 +107,27 @@ char *augustus_decrypt(char *cipher, char *key) {
     char *decrypted= malloc(sizeof(cipher));
 
     for (int i = 0; i < cipherLen; i++) {
-        char currentChar = decrypted;
+        char currentChar = cipher[i];
         char newChar;
-        int curKey = atoi(key[i%keyLen]);
+        
+        char temp;
+        temp = key[i%keyLen];
+        int val = temp - 48;
 
-        newChar = caesarRevert(currentChar, curKey);
+
+
+        newChar = caesarRevert(currentChar, val);
         decrypted[i] = newChar;
 
     }
 
     for (int i = 0; i < cipherLen; i++) {
-        char currentChar = decrypted;
+        char currentChar = decrypted[i];
         char newChar;
 
         newChar = caesarRevert(currentChar, intKey);
         decrypted[i] = newChar;
+    }
+
+    return decrypted;
 }
--- augustus.h
+++ augustus.h
@@ -2,5 +2,7 @@
 #define _AUGUSTUS_H
 char *augustus_encrypt(char *plain, char *key);
 char *augustus_decrypt(char *cipher, char *key);
-
+char caesarConvert(char input, int key);
+char caesarRevert(char input, int key);
+int wrapAround(int start, int key);
 #endif // _AUGUSTUS_H
